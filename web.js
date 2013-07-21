b4dc4ca15b961eed35b25cbf3b5a86c0f7ea247b
Ramil Avraham <rami.av@hotmail.com>
2013-07-21 17:08:00 +0000

    updated the static file

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var  fs = require('fs');
+var fs = require('fs');
+var htmlfile = "index.html";
+
 
 var app = express.createServer(express.logger());
-var file = fs.readFileSync("index.html");
-var buffer = new Buffer ( file , "utf-8")
+
 
 app.get('/', function(request, response) {
-    
-  response.send(buffer.toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
+app.use("/static",express.static(__dirname+"/static"));
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
